b390d066b8e44a9c7ce4fa18a15e57a0aab3c183
yomiannixs-jpg <yomiannixs@gmail.com>
2026-03-31 18:19:00 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
-  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY",
+  supabaseUrl: "https://rwfcgjhgenoicvarpzze.supabase.co",
+  supabaseAnonKey: "sb_publishable_87HpptbyngKRTrw2o6lqFg_bL8A_jwn",
   adminRedirectAfterLogin: "./admin.html"
 };
